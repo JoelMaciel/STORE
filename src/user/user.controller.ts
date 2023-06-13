c6b1e98c7d45df157b0aad2a431981c873d0d9cf
Joel Maciel <soldado25461@gmail.com>
2023-06-12 22:55:44 -0300

    feat: creating user module

---
+++ src/user/user.controller.ts
@@ -0,0 +1,19 @@
+/* eslint-disable prettier/prettier */
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UserRepository } from './user.repository';
+
+@Controller('/users')
+export class UserController {
+  constructor(private userRepository: UserRepository) {}
+
+  @Post()
+  async createUser(@Body() userData) {
+    this.userRepository.save(userData);
+    return userData;
+  }
+
+  @Get()
+  async listUsers() {
+    return this.userRepository.list();
+  }
+}
---
+++ src/user/user.repository.ts
@@ -0,0 +1,16 @@
+/* eslint-disable prettier/prettier */
+
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UserRepository {
+  private users = [];
+
+  async save(user) {
+    this.users.push(user);
+  }
+
+  async list() {
+    return this.users;
+  }
+}
